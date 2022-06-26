02bcbbd61c6c73793810d2f516df0dff787dbc91
imun <imun22@gmail.com>
2022-06-26 17:15:07 +0430

    Add ResultStatus enum

---
+++ src/iman.Result/ResultStatus.cs
@@ -0,0 +1,12 @@
+namespace iman.Result
+{
+    public enum ResultStatus
+    {
+        Unknown = -1,
+        Ok = 0,
+        Error = 1,
+        Invalid = 2,
+        NotFound = 3,
+        Unauthorized = 4
+    }
+}
